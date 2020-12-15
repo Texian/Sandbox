469ae82b789c00e846232826613c65d2c72c4639
cwalters <xian04@yahoo.com>
2020-12-14 17:09:41 -0800

    'blade' and 'content' divs are properly stacking over each other

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,12 @@
 let wrappers = Array.from(document.getElementsByClassName("bladeWrapper"));
-
 let blades = Array.from(document.getElementsByClassName("blade"));
-    
+//.getElements returns as an HTMLCollection and must be converted to an array  
+
+//First, remove any existing 'active' classes, then add 'active' to the clicked element
 function toggleActive(evt) {
     blades.forEach(blade => blade.classList.remove("active"));
     evt.target.classList.add("active");
 }
 
+//Add the eventListener to all of the wrapper elements
 wrappers.forEach(wrapper => wrapper.addEventListener('click', toggleActive));
