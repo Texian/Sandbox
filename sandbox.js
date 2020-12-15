d49eb2525563c1493e1c49a5f62e7e8e3df49ba2
cwalters <xian04@yahoo.com>
2020-12-15 14:15:21 -0800

    Fully functional 'blades' module

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,14 @@
+// Get the needed div elements
 let wrappers = Array.from(document.getElementsByClassName("bladeWrapper"));
 let blades = Array.from(document.getElementsByClassName("blade"));
 //.getElements returns as an HTMLCollection and must be converted to an array  
 
-//First, remove any existing 'active' classes, then add 'active' to the clicked element
+//First, remove any existing 'active' classes, then add 'active' to the clicked element if it's a 'blade' element
 function toggleActive(evt) {
     blades.forEach(blade => blade.classList.remove("active"));
-    evt.target.classList.add("active");
+    if (evt.target.classList.contains("blade")) {
+        evt.target.classList.add("active");
+    }
 }
 
 //Add the eventListener to all of the wrapper elements
